f5a1297f26d398082a4e3db4d48c5a01971c9dee
Dima <pushokf@gmail.com>
2023-11-05 17:43:42 +0200

    cookie parser

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const userRoute = require("./routes/userRoute");
 const errorHandler = require("./middleware/errorHandler");
@@ -12,8 +13,10 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 // Routes middleware
 app.use("/api/users", userRoute);
