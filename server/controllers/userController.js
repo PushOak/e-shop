8f44b54dc3b02192deaaf715892a8d5ef12708b3
Dima <pushokf@gmail.com>
2023-11-05 16:54:34 +0200

    userController - register new user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,51 @@
+const asyncHandler = require("express-async-handler"); // removes the need to type try/catch block everytime
+const User = require("../models/userModel");
 
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
 
-const registerUser = (req, res) => {
-    if (!req.body.email) {
-        res.status(400)
-        throw new Error("Please add an email.");
+    // Validation
+    if (!name || !email || !password) {
+        res.status(400);
+        throw new Error("Please fill in all of the required fields.");
     }
-    res.send("Register user!");
-};
+
+    if (password.length < 6) {
+        res.status(400);
+        throw new Error("The password must be up to 6 characters.");
+    }
+
+    // Check if user email already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        res.status(404);
+        throw new Error("This email already exists.");
+    }
+
+    // Create new user
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+
+    if (user) {
+        const { _id, name, email, photo, phone, bio } = user;
+        res.status(201).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Ivalid user data!");
+    }
+});
 
 module.exports = {
     registerUser
-}
+};
