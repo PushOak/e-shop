12defc9cfdc0499471862b185030a8467f866707
Dima <pushokf@gmail.com>
2023-11-06 10:07:27 +0200

    userController - line 98 bug fix

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -95,14 +95,16 @@ const loginUser = asyncHandler(async (req, res) => {
     // Generate token
     const token = generateToken(user._id);
 
-    // Send HTTP-only cookie 
-    res.cookie("token", token, {
-        path: "/",
-        httpOnly: true,
-        expires: new Date(Date.now() + 1000 * 86400), // 1 day
-        sameSite: "none",
-        secure: true,
-    })
+    if(passwordIsCorrect){
+        // Send HTTP-only cookie 
+        res.cookie("token", token, {
+            path: "/",
+            httpOnly: true,
+            expires: new Date(Date.now() + 1000 * 86400), // 1 day
+            sameSite: "none",
+            secure: true,
+        });
+    }
 
     if (user && passwordIsCorrect) {
         const { _id, name, email, photo, phone, bio } = user;
