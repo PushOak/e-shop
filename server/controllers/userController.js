4ee1774afbc39351a295fd742f98e0104619a3d1
Dima <pushokf@gmail.com>
2023-11-06 10:02:45 +0200

    user login functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -73,7 +73,52 @@ const registerUser = asyncHandler(async (req, res) => {
 
 // Login existing user
 const loginUser = asyncHandler(async (req, res) => {
-    res.send("Login user");
+    const { email, password } = req.body;
+
+    // Validate request
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("Please add an email and password.");
+    };
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        res.status(404);
+        throw new Error("User not found, please sign up.");
+    };
+
+    // If user exists, check if the password is correct
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
+
+    // Generate token
+    const token = generateToken(user._id);
+
+    // Send HTTP-only cookie 
+    res.cookie("token", token, {
+        path: "/",
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000 * 86400), // 1 day
+        sameSite: "none",
+        secure: true,
+    })
+
+    if (user && passwordIsCorrect) {
+        const { _id, name, email, photo, phone, bio } = user;
+        res.status(200).json({
+            _id,
+            name,
+            email,
+            photo,
+            phone,
+            bio,
+            token,
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid email or password.");
+    }
 });
 
 module.exports = {
