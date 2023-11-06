5ee60e06e950455f3c38ed65a750eb41eff88afb
Dima <pushokf@gmail.com>
2023-11-06 11:32:27 +0200

    protect user

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const {
     registerUser,
     loginUser,
-    logout
+    logout,
+    getUser,
 } = require("../controllers/userController");
+const protect = require("../middleware/authMiddleware");
 
 // Auth route (user registration)
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
+router.get("/getUser", protect, getUser);
 
 module.exports = router;
