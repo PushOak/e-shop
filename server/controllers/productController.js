d8a294db0e03a3b0ce0fec33cb519257598a8d8c
Dima <pushokf@gmail.com>
2023-11-10 15:02:15 +0200

    createProduct function and productModel bug fix

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,9 +1,31 @@
 const asyncHandler = require("express-async-handler");
+const Product = require("../models/productModel");
 
 const createProduct = asyncHandler(async (req, res) => {
-    res.send("Created a Product");
+    const { name, sku, category, quantity, price, description } = req.body;
+
+    // Product validation
+    if (!name || !category || !quantity || !price || !description) {
+        res.status(400);
+        throw new Error("Please fill in all of the fields.");
+    }
+
+    // Manage Image upload
+
+    // Create a single product
+    const product = await Product.create({
+        user: req.user.id,
+        name,
+        sku,
+        category,
+        quantity,
+        price,
+        description,
+    });
+
+    res.status(201).json(product)
 });
 
 module.exports = {
     createProduct,
-}
+};
--- server/models/productModel.js
+++ server/models/productModel.js
@@ -47,5 +47,5 @@ const productSchema = mongoose.Schema({
     }
 );
 
-const Token = mongoose.model("Product", productSchema);
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
