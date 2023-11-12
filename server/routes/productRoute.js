00d843ab17ec6ec44887be884e8e86878e33e2b2
Dima <pushokf@gmail.com>
2023-11-12 12:15:58 +0200

    get a single product by id

--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -4,10 +4,12 @@ const protect = require("../middleware/authMiddleware");
 const {
     createProduct,
     getProducts,
+    getSingleProduct,
 } = require("../controllers/productController");
 const { upload } = require("../utils/fileUpload");
 
 router.post("/", protect, upload.single("image"), createProduct);
 router.get("/all-products", protect, getProducts);
+router.get("/:id", protect, getSingleProduct);
 
 module.exports = router;
