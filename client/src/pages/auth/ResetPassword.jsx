3e981f828a4c79d39fa5d31f2e1caf9950e0035f
Dima <pushokf@gmail.com>
2023-11-14 13:00:31 +0200

    Reset password page layout

--- client/src/pages/auth/ResetPassword.jsx
+++ client/src/pages/auth/ResetPassword.jsx
@@ -1,11 +1,52 @@
 import React from "react";
+import styles from "./auth.module.scss";
+import { MdPassword } from "react-icons/md";
+import Card from "../../components/card/Card";
+import { Link } from "react-router-dom";
 
 export default function ResetPassword() {
     return (
         <>
-            <div>
-                ResetPassword
+            <div className={`container ${styles.auth}`}>
+                <Card>
+                    <div className={styles.form}>
+                        <div className="--flex-center">
+                            <MdPassword size={35} color="#997" />
+                        </div>
+                        <h2>Reset Password</h2>
+
+                        <form>
+                            <input
+                                type="password"
+                                placeholder="New password"
+                                name="password"
+                                required
+                            />
+                            <input
+                                type="password"
+                                placeholder="Confirm new password"
+                                name="password"
+                                required
+                            />
+                            <button
+                                type="submit"
+                                className="--btn --btn-primary --btn-block"
+                            >
+                                Reset Password
+                            </button>
+                            <div className={styles.links}>
+                                <p>
+                                    <Link to="/">- Home</Link>
+                                </p>
+                                <p>
+                                    <Link to="/login" >- Login</Link>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </Card>
             </div>
         </>
     );
 };
+
