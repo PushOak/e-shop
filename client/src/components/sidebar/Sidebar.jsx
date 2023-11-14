9a8f1124758676a77573fcfccb18bcc505d986fd
Dima <pushokf@gmail.com>
2023-11-14 13:35:55 +0200

    sidebar component

---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./sidebar.scss";
+
+export default function Sidebar() {
+    return (
+        <>
+            <div>
+                Sidebar
+            </div>
+        </>
+    );
+};
---
+++ client/src/components/sidebar/SidebarItem.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function SidebarItem() {
+    return (
+        <>
+            <div>
+                Sidebar Item
+            </div>
+        </>
+    );
+};
