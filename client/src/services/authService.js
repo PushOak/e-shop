b082d7a13048420b49ea76ea5c8e6e95ed56e77a
Dima <pushokf@gmail.com>
2023-11-15 19:27:09 +0200

    loginUser in authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -11,11 +11,34 @@ export const validateEmail = (email) => {
     return email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 };
 
+// Register new user
 export const registerUser = async (userData) => {
     try {
         const response = await axios.post(`${SERVER_URL}/api/auth/register`, userData, { withCredentials: true });
         if (response.statusText === "OK") {
             toast.success("User regsitered successfully!");
+        };
+        return response.data;
+    } catch (error) {
+        const message = (
+            error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+            error.message ||
+            error.toString();
+        toast.error(message);
+    };
+};
+
+// Login existing user
+export const loginUser = async (userData) => {
+    try {
+        const response = await axios.post(
+            `${SERVER_URL}/api/auth/login`,
+            userData,
+        );
+        if (response.statusText === "OK") {
+            toast.success("Logged in successfully!");
         };
         return response.data;
     } catch (error) {
