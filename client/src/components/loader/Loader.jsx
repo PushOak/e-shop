36fb626114c596b155a215a3eb1db0728be746e4
Dima <pushokf@gmail.com>
2023-11-15 19:22:47 +0200

    Loader.jsx funcitonality

--- client/src/components/loader/Loader.jsx
+++ client/src/components/loader/Loader.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./loader.scss";
+import loaderImage from "../../assets/loader.gif";
+import ReactDOM from "react-dom";
 
 export default function Loader() {
+    return ReactDOM.createPortal(
+        <div className="wrapper">
+            <div className="loader">
+                <img src={loaderImage} alt="loading..." />
+            </div>
+        </div>,
+        document.getElementById("loader")
+    );
+};
+
+export const SpinnerImage = () => {
     return (
-        <>
-            <div>Loader</div>
-        </>
+        <div className="--center-all">
+            <img src={loaderImage} alt="loading..." />
+        </div>
     );
 };
