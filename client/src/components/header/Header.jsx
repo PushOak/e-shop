63a001d77972856689e9fed3de2488971836633d
Dima <pushokf@gmail.com>
2023-11-16 10:39:16 +0200

    Display name of logged in user in header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { logoutUser } from "../../services/authService";
-import { useDispatch } from "react-redux";
-import { SET_LOGIN } from "../../redux/features/auth/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { SET_LOGIN, selectName } from "../../redux/features/auth/auth/authSlice";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const name = useSelector(selectName);
 
     const logout = async () => {
         await logoutUser();
@@ -19,8 +20,8 @@ export default function Header() {
             <div className="--pad header">
                 <div className="--flex-between">
                     <h3>
-                        <span className="--fw-thin">Welcome,</span>
-                        <span className="--color-danger">Dima</span>
+                        <span className="--fw-thin">Welcome, </span>
+                        <span className="--color-danger">{name}</span>
                     </h3>
                     <button className="--btn --btn-danger" onClick={logout}>Logout</button>
                 </div>
