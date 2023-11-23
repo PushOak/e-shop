172bc318f9b4c3edecf3f73edd66f6061e85a6f2
Dima <pushokf@gmail.com>
2023-11-23 17:52:05 +0200

    delete product in redux set up

--- client/src/redux/features/product/productService.js
+++ client/src/redux/features/product/productService.js
@@ -16,9 +16,16 @@ const getProducts = async () => {
     return response.data;
 };
 
+// Delete a product
+const deleteProduct = async (id) => {
+    const response = await axios.delete(API_URL + id);
+    return response.data;
+};
+
 const productService = {
     createProduct,
     getProducts,
+    deleteProduct,
 };
 
 export default productService;
